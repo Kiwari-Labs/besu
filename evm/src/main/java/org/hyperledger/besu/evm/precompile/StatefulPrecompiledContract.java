627a7bcd90cc2af7958e1264f353f6c66e85a429
MASDXI <sirawitt42@gmail.com>
2024-09-16 12:24:09 +0700

    feat: erc20 precompile
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

--- evm/src/main/java/org/hyperledger/besu/evm/precompile/StatefulPrecompiledContract.java
+++ evm/src/main/java/org/hyperledger/besu/evm/precompile/StatefulPrecompiledContract.java
@@ -22,14 +22,42 @@ public class StatefulPrecompiledContract extends AbstractPrecompiledContract {
     
     private static final Logger LOG = LoggerFactory.getLogger(StatefulPrecompiledContract.class);
 
+    // temporary use `0x0000000000000000000000000000000000000802` same as in moonbeam network.
+    private static final Address IERC20_ADDRESS = Address.fromHexString("0x0000000000000000000000000000000000000802");
+
+    private static final Bytes APPROVE_SIGNATURE = Hash.keccak256(Bytes.of("approve(address,uint256)".getBytes(UTF_8))).slice(0, 4);
+    private static final Bytes ALLOWANCE_SIGNATURE = Hash.keccak256(Bytes.of("allowance(address,address)".getBytes(UTF_8))).slice(0, 4);
+    private static final Bytes BALANCEOF_SIGNATURE = Hash.keccak256(Bytes.of("balanceOf(address)".getBytes(UTF_8))).slice(0, 4);
+    private static final Bytes DECIMALS_SIGNATURE = Hash.keccak256(Bytes.of("decimals()".getBytes(UTF_8))).slice(0, 4);
+    private static final Bytes NAME_SIGNATURE = Hash.keccak256(Bytes.of("name()".getBytes(UTF_8))).slice(0, 4);
+    private static final Bytes SYMBOL_SIGNATURE = Hash.keccak256(Bytes.of("symbol()".getBytes(UTF_8))).slice(0, 4);
+    private static final Bytes TRANSFER_SIGNATURE = Hash.keccak256(Bytes.of("transfer(address,uint256)".getBytes(UTF_8))).slice(0, 4);
+    private static final Bytes TRANSFERFROM_SIGNATURE = Hash.keccak256(Bytes.of("transferFrom(address,address,uint256)".getBytes(UTF_8))).slice(0, 4);
+    private static final Bytes TOTALSUPPLY_SIGNATURE = Hash.keccak256(Bytes.of("totalSupply()".getBytes(UTF_8))).slice(0, 4);
+
+    // Transfer(address, address, uint256)
+    // Approval(address, address, uint256)
+
+    // Retrieving ERC20 data directly from the storage slot will return 0x0 or empty 
+    // because the data is hardcoded and not stored in the database.
+    // Only the approval data can be retrieved from the storage slot.
+
     public StatefulPrecompiledContract(final GasCalculator gasCalculator) {
         super("StatefulPrecompiledContract", gasCalculator);
     }
 
     @Override
     public long gasRequirement(final Bytes input) {
-        // @TODO change reasonable gas calculate.
-        return 1000L; // Define gas cost
+        final Bytes function = input.slice(0, 4);
+        if (function.equals(APPROVE_SIGNATURE) ||
+            function.equals(TRANSFER_SIGNATURE) ||
+            function.equals(TRANSFERFROM_SIGNATURE)) {
+            // gas used for write to database
+            return 3000;
+        } else {
+            // gas used for read from database
+            return 1000;
+        }
     }
 
     @Nonnull
@@ -41,16 +69,16 @@ public class StatefulPrecompiledContract extends AbstractPrecompiledContract {
           null, Optional.of(ExceptionalHaltReason.PRECOMPILE_ERROR));
         } else { 
             final WorldUpdater worldUpdater = messageFrame.getWorldUpdater();
-            final MutableAccount mutableAccount = worldUpdater.getOrCreate(Address.fromHexString("0x0100000000000000000000000000000000000001"));
-            mutableAccount.setStorageValue(UInt256.ZERO, UInt256.valueOf(1337));
+            final MutableAccount erc20 = worldUpdater.getOrCreate(IERC20_ADDRESS);
+            // erc20.setStorageValue(UInt256.ZERO, UInt256.valueOf(1337));
             // final Bytes32 storedValue = mutableAccount.getStorageValue(UInt256.ZERO);
             
             // ignore input, just increment balance with fixed value 1000 wei each call.
-            mutableAccount.incrementBalance(Wei.of(1337));
+            erc20.incrementBalance(Wei.of(1337));
 
             worldUpdater.commit();
             messageFrame.storageWasUpdated(UInt256.ZERO, UInt256.valueOf(1337));
-            LOG.info("State updated balance successfully: {}", mutableAccount.getBalance());
+            LOG.info("State updated balance successfully: {}", erc20.getBalance());
             return PrecompileContractResult.success(input.copy());
         }
     }
